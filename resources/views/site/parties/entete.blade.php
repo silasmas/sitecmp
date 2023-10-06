60d317db5b149ee147ea0c05474389dcea8e8976
silasmas <silasjmas@gmail.com>
2023-10-06 22:11:52 +0100

    dkd

--- resources/views/site/parties/entete.blade.php
+++ resources/views/site/parties/entete.blade.php
@@ -4,8 +4,8 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="keywords" content="HTML5 Template" />
-<meta name="description" content="Webster - Responsive Multi-purpose HTML5 Template" />
-<meta name="author" content="potenzaglobalsolutions.com" />
+<meta name="description" content="Portail web de l'église Centre Missionnaire Philadelphie" />
+<meta name="author" content="silasmas.com" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <title>{{ config('app.name') }} |
