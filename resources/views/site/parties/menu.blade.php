e37609caadac27a3016e9728c16427f786a4767c
Jeanpy NTMB <78202651+pedrien@users.noreply.github.com>
2023-10-06 10:27:53 +0100

    Revert "ok pedro"
    This reverts commit 6f1e0dc91b67ddacb5f9f1364027d221e77beb83.

--- resources/views/site/parties/menu.blade.php
+++ resources/views/site/parties/menu.blade.php
@@ -1,7 +1,7 @@
 <header id="header" class="header default">
     <div class="topbar">
         <div class="container">
-            <div class="row">
+            <div class="row align-items-center">
                 <div class="col-lg-6 col-md-6 xs-mb-10">
                     <div class="topbar-call text-center text-md-start">
                         <ul>
@@ -13,7 +13,7 @@
                 </div>
                 <div class="col-lg-6 col-md-6">
                     <div class="topbar-social text-center text-md-end">
-                        <ul>
+                        <ul class="d-flex align-items-center justify-content-end">
                             <li><a href="#"><span class="ti-facebook"></span></a></li>
                             <li><a href="#"><span class="ti-instagram"></span></a></li>
                             <li><a href="#"><span class="ti-twitter"></span></a></li>
@@ -32,25 +32,39 @@
             <!-- menu list items container -->
             <section class="menu-list-items">
                 <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12 col-md-12 position-relative">
+                    <div class="row align-items-center">
+                        <div class="col-lg-2 col-md-12 position-relative">
                             <!-- menu logo -->
                             <ul class="menu-logo">
                                 <li>
-                                    <a href="index.html"><img id="logo_img"
+                                    <a href="{{ route('home') }}"><img id="logo_img"
                                             src="{{ asset('assets/site/images/logo.png') }}" alt="logo"> </a>
                                 </li>
                             </ul>
+                            
+                        </div>
+                        <div class="col-lg-10">
                             <!-- menu links -->
-                            <div class="menu-bar">
-                                <ul class="menu-links">
+                            <div class="menu-bar d-flex align-items-center">
+                                <ul class="menu-links d-lg-flex align-items-center">
                                     <li
-                                        class="{{ Route::current()->getName() == 'home' ? 'active' : '' }}">
-                                        <a href="index.html">@lang('miscellaneous.main_menu.home') </a></li>
+                                        class="{{ Route::current()->getName() == 'home' ? 'active' : ''}}">
+                                        <a href="{{ route('home') }}">@lang('miscellaneous.main_menu.home') </a></li>
 
-                                    <li class="hoverTrigger {{ Route::current()->getName() == 'about' ? 'active' : '' }}">
-                                        <a href="javascript:void(0)">@lang('miscellaneous.main_menu.who_are_we.about')
-                                            <i class="fa fa-angle-down fa-indicator"></i> </a>
+                                    <li class="hoverTrigger {{ Route::current()->getName() == 'about' ? 'active' : ''}}">
+                                        
+                                            <div class="dropdown">
+                                                <a href="javascript:void(0)" class="link-drop" type="button" data-bs-toggle="dropdown" aria-expanded="false">@lang('miscellaneous.main_menu.who_are_we.about')
+                                                    <i class="fa fa-angle-down fa-indicator"></i> </a>
+                                                <ul class="dropdown-menu">
+                                                    <li><a class="dropdown-item" href="{{ route('about') }}">Qui sommes-nous</a></li>
+                                                    <li>
+                                                        <a class="dropdown-item" href="{{ route('galerie') }}" >
+                                                            @lang('miscellaneous.main_menu.gallery') 
+                                                        </a>
+                                                    </li>
+                                                </ul>
+                                            </div>
                                     </li>
                                     <!-- drop down full width -->
                                     <div class="drop-down grid-col-12">
@@ -70,26 +84,47 @@
                                             </div>
                                         </div>
                                     </div>
+                                    <li class="hoverTrigger {{ Route::current()->getName() == 'articles' ? 'active' : ''}}">
+                                        
+                                        <div class="dropdown">
+                                            <a href="javascript:void(0)" class="link-drop" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                Méditation
+                                                <i class="fa fa-angle-down fa-indicator"></i> </a>
+                                            <ul class="dropdown-menu">
+                                                <li><a class="dropdown-item" href="{{ route('articles') }}">@lang('miscellaneous.main_menu.news')</a></li>
+                                                <li>
+                                                    <a class="dropdown-item" href="#">
+                                                        Vidéos
 
-                                    <li class=" {{ Route::current()->getName() == 'articles' ? 'active' : '' }}"><a
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                </li>
+                                    {{-- <li class=" {{ Route::current()->getName() == 'articles' ? 'active' : '' }}"><a
                                             href="{{ route('articles') }}">@lang('miscellaneous.main_menu.news') </a>
-                                    </li>
+                                    </li> --}}
                                     <li class=" {{ Route::current()->getName() == 'events' ? 'active' : '' }}"><a
                                             href="{{ route('events') }}">@lang('miscellaneous.main_menu.events') </a>
                                     </li>
                                     <li class=" {{ Route::current()->getName() == 'projects' ? 'active' : '' }}"><a
                                             href="{{ route('projects') }}">@lang('miscellaneous.main_menu.projects')
                                         </a></li>
-                                    <li class=" {{ Route::current()->getName() == 'contributions' ? 'active' : '' }}"><a
+                                    {{-- <li class=" {{ Route::current()->getName() == 'contributions' ? 'active' : '' }}"><a
                                             href="{{ route('contributions') }}">@lang('miscellaneous.main_menu.contribution')
-                                        </a></li>
-                                    <li class=" {{ Route::current()->getName() == 'galerie' ? 'active' : '' }}"><a
-                                            href="{{ route('galerie') }}">@lang('miscellaneous.main_menu.gallery') </a>
+                                        </a></li> --}}
+                                    <li class=" {{ Route::current()->getName() == 'galerie' ? 'active' : '' }} link"><a
+                                            href="#">
+                                            Bunda 21
+                                        </a>
                                     </li>
-                                    <li class=" {{ Route::current()->getName() == 'contact' ? 'active' : '' }}"><a
+                                    <li class=" {{ Route::current()->getName() == 'contact' ? 'active' : '' }} link"><a
                                             href="{{ route('contact') }}">@lang('miscellaneous.main_menu.who_are_we.contact')
                                         </a></li>
                                 </ul>
+                                <a href="{{ route('contributions') }}" class="btn btn-primary btn-uppertext">
+                                    @lang('miscellaneous.main_menu.contribution')
+                                </a>
                                 <div class="search-cart">
                                     <div class="shpping-cart">
                                         <a class="cart-btn" href="#"> <i class="fa fa-language icon"
