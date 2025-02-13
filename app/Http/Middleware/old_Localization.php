07c7e1b7133d8c615450858f3fc33aace224d72a
silasmas <silasjmas@gmail.com>
2025-02-13 15:59:17 +0100

    fgf

--- app/Http/Middleware/old_Localization.php
+++ app/Http/Middleware/old_Localization.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Session;
 
-class Localization
+class old_Localization
 {
     /**
      * Handle an incoming request.
