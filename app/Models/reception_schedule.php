3cde530957fa3873c386a34dfb57f565220f0132
silasmas <silasjmas@gmail.com>
2025-02-13 16:46:22 +0100

    Update reception_schedule.php

--- app/Models/reception_schedule.php
+++ app/Models/reception_schedule.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class reception_schedule extends Model
+class Reception_schedule extends Model
 {
     use HasFactory;
     protected $guarded = [];
